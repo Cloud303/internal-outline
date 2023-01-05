874bb4ea502ef145c6f4cdb3b7d5e07d7ee27ff5
cloud303-eAli <eali@cloud303.io>
2023-01-05 15:18:50 +0500

    fix duplicate heading in TOC

--- shared/editor/components/TableOfContents.tsx
+++ shared/editor/components/TableOfContents.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import headingToSlug from "../lib/headingToSlug";
 
 const TableOfContents = ({ editor, node }: { editor: any; node: any }) => {
   const { view } = editor;
   const { doc } = view.state;
+  const previouslySeen = {};
 
   const headings = [];
   for (let i = 0; i < doc.nodeSize - 2; i++) {
@@ -17,10 +19,21 @@ const TableOfContents = ({ editor, node }: { editor: any; node: any }) => {
   return (
     <ul>
       {headings?.map((heading) => {
-        console.log(heading);
+        const slug = headingToSlug(heading);
+
+        previouslySeen[slug] =
+          previouslySeen[slug] !== undefined ? previouslySeen[slug] + 1 : 0;
+
+        console.log(previouslySeen);
         return (
           <li key={heading.textContent}>
-            <a href={`#h-${heading.textContent?.toLowerCase()}`}>
+            <a
+              href={`#${
+                previouslySeen[slug] > 0
+                  ? headingToSlug(heading, previouslySeen[slug])
+                  : headingToSlug(heading)
+              }`}
+            >
               {heading.textContent}
             </a>
           </li>
