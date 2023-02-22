c103901955775bf215cfd2d78affb2c290a5ce64
EhtashamAli <ehtasham.ali1979@gmail.com>
2023-02-23 01:02:58 +0500

    Update group and member permissions

--- server/policies/team.ts
+++ server/policies/team.ts
@@ -1,7 +1,12 @@
 import { Team, User } from "@server/models";
 import { allow } from "./cancan";
 
-allow(User, "read", Team, (user, team) => user.teamId === team?.id);
+allow(User, "read", Team, (user, team) => {
+  if (user.isViewer || user.teamId !== team?.id) {
+    return false;
+  }
+  return true;
+});
 
 allow(User, "share", Team, (user, team) => {
   if (!team || user.isViewer || user.teamId !== team.id) {
