d92391dd8748dcc348389b9025f5be207b11ce2e
cloud303-eAli <eali@cloud303.io>
2023-09-06 04:21:01 +0500

    migration fix

--- server/migrations/1685466531-add-coverImg-position-fields.js
+++ server/migrations/1685466531-add-coverImg-position-fields.js
@@ -1,11 +1,11 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn("documents", "coverImgPositionX", {
-      type: Sequelize.INTEGER,
+      type: "INTEGER",
       allowNull: true,
     });
     await queryInterface.addColumn("documents", "coverImgPositionY", {
-      type: Sequelize.INTEGER,
+      type: "INTEGER",
       allowNull: true,
     });
   },
